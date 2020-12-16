5d3663194d02531022619500a78f20251999aaee
jfspps <jfsapps@gmail.com>
2020-12-16 17:56:26 +0000

    bound outputCount to appCount

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularDatabindingExercise';
+
+  appCount: number = 0;
+
+  // note the name outputValue must match that given in the @Output
+  onCountChange(count: {outputValue: number}){
+    this.appCount = count.outputValue;
+  }
+
 }
